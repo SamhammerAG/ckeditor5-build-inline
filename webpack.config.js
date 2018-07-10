486d59768a0fc33dd94b9cdda22f56cb18f69010
rschmitt <rschmitt@samhammer.de>
2018-07-10 10:13:14 +0200

    added extract-text-webpack-plugin to generate css file

--- webpack.config.js
+++ webpack.config.js
@@ -7,20 +7,21 @@
 
 /* eslint-env node */
 
-const path = require( 'path' );
-const webpack = require( 'webpack' );
-const { bundler, styles } = require( '@ckeditor/ckeditor5-dev-utils' );
-const CKEditorWebpackPlugin = require( '@ckeditor/ckeditor5-dev-webpack-plugin' );
-const BabiliPlugin = require( 'babel-minify-webpack-plugin' );
-const buildConfig = require( './build-config' );
+const path = require('path');
+const webpack = require('webpack');
+const { bundler, styles } = require('@ckeditor/ckeditor5-dev-utils');
+const CKEditorWebpackPlugin = require('@ckeditor/ckeditor5-dev-webpack-plugin');
+const BabiliPlugin = require('babel-minify-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const buildConfig = require('./build-config');
 
 module.exports = {
 	devtool: 'source-map',
 
-	entry: path.resolve( __dirname, 'src', 'ckeditor.js' ),
+	entry: path.resolve(__dirname, 'src', 'ckeditor.js'),
 
 	output: {
-		path: path.resolve( __dirname, 'build' ),
+		path: path.resolve(__dirname, 'build'),
 		filename: 'ckeditor.js',
 		libraryTarget: 'umd',
 		libraryExport: 'default',
@@ -28,17 +29,18 @@ module.exports = {
 	},
 
 	plugins: [
-		new CKEditorWebpackPlugin( {
+		new CKEditorWebpackPlugin({
 			language: buildConfig.config.language,
 			additionalLanguages: 'all'
-		} ),
-		new BabiliPlugin( null, {
+		}),
+		new BabiliPlugin(null, {
 			comments: false
-		} ),
-		new webpack.BannerPlugin( {
+		}),
+		new webpack.BannerPlugin({
 			banner: bundler.getLicenseBanner(),
 			raw: true
-		} ),
+		}),
+		new ExtractTextPlugin('ckeditor.css'),
 		new webpack.optimize.ModuleConcatenationPlugin()
 	],
 
@@ -46,27 +48,24 @@ module.exports = {
 		rules: [
 			{
 				test: /\.svg$/,
-				use: [ 'raw-loader' ]
+				use: ['raw-loader']
 			},
 			{
 				test: /\.css$/,
-				use: [
-					{
-						loader: 'style-loader',
-						options: {
-							singleton: true
+				use: ExtractTextPlugin.extract({
+					fallback: 'style-loader',
+					use: [
+						{
+							loader: 'postcss-loader',
+							options: styles.getPostCssConfig({
+								themeImporter: {
+									themePath: require.resolve('@ckeditor/ckeditor5-theme-lark')
+								},
+								minify: true
+							})
 						}
-					},
-					{
-						loader: 'postcss-loader',
-						options: styles.getPostCssConfig( {
-							themeImporter: {
-								themePath: require.resolve( '@ckeditor/ckeditor5-theme-lark' )
-							},
-							minify: true
-						} )
-					},
-				]
+					]
+				})
 			}
 		]
 	}
